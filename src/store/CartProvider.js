6c6c74113a86ef5db7bfe94dc3da3aa8ed0553c1
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-10-07 18:18:26 +0200

    Added Cart Context Provider component

---
+++ src/store/CartProvider.js
@@ -0,0 +1,18 @@
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {};
+
+  const removeItemFromCartHandler = (id) => {};
+
+  const cartContext = {
+    item: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return <CartContext.Provider value={CartContext}>{props.children}</CartContext.Provider>;
+};
+
+export default CartProvider;
