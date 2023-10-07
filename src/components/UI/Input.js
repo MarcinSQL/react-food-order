e82db9e4b2fd505e5fc4e11b6807edba01e7ac49
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-10-07 17:31:53 +0200

    Added Input component and used it in MealItemForm

---
+++ src/components/UI/Input.js
@@ -0,0 +1,11 @@
+import classes from './Input.module.css';
+
+const Input = props => {
+    return <div className={classes.input}>
+        <label>{props.label}
+            <input {...props.input}  />
+        </label>
+    </div>
+};
+
+export default Input;
