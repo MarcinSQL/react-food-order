414dff1fe2e3e5547b0fa04d9bdf6e76eaf003f9
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-10-07 18:20:54 +0200

    Used CartProvider on App module

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 const App = () => {
   const [cartIsVisible, setCartIsVisible] = useState(false);
@@ -15,13 +16,13 @@ const App = () => {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsVisible && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 };
 
