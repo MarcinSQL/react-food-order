04635c039a3d51153ed531731882dbe7ba5b6485
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-10-08 23:49:07 +0200

    Added removeItem feature to CartProvider

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,9 +11,13 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const onRemoveHandler = (id) => {};
+  const onRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const onAddHandler = (item) => {};
+  const onAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
